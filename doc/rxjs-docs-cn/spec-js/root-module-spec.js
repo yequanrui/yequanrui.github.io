157e707df6eaa9af42775e896966346546baaa79
yequanrui <yequanrui@qq.com>
2020-10-22 00:54:18 +0800

    delete rxjs-doc-cn

--- doc/rxjs-docs-cn/spec-js/root-module-spec.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Rx = require('../dist/cjs/Rx');
-describe('Root Module', function () {
-    it('should contain exports from commonjs modules', function () {
-        expect(typeof Rx.Observable).toBe('function');
-    });
-});
-//# sourceMappingURL=root-module-spec.js.map
